0de8b48e07337fe8e731a53d69070c9672977faf
Paulo Ricardo M <pr741234@gmail.com>
2024-07-15 17:55:09 -0300

    feat: Implementa produtor da mensagem

---
+++ RMQ-Fanout/Producer/Program.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+for (int i = 0; i < 10; i++)
+{
+	string message = $"Log message {i}";
+	var body = Encoding.UTF8.GetBytes(message);
+
+	channel.BasicPublish(
+		exchange: "logs",
+		routingKey: "",
+		basicProperties: null,
+		body: body);
+
+	Console.WriteLine($"[x] Sent {message}");
+}
+
+Console.WriteLine("Press [Enter] to exit.");
+Console.ReadLine();
