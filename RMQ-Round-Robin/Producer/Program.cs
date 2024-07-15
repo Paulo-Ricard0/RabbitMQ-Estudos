bc5c9adde7fdf73aa0bd72323bc0ef50fc6d52fe
Paulo Ricardo M <pr741234@gmail.com>
2024-07-15 15:46:02 -0300

    feat: Implementa produtor da mensagem RabbitMQ

---
+++ RMQ-Round-Robin/Producer/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+	queue: "hello",
+	durable: true,
+	exclusive: false,
+	autoDelete: false,
+	arguments: null);
+
+for (int i = 0; i < 10; i++)
+{
+	string message = $"Message {i}";
+	var body = Encoding.UTF8.GetBytes(message);
+
+	var properties = channel.CreateBasicProperties();
+	properties.Persistent = true;
+
+	channel.BasicPublish(
+		exchange: "",
+		routingKey: "hello",
+		basicProperties: properties,
+		body: body);
+
+	Console.WriteLine($"[x] Sent {message}");
+}
+
+Console.WriteLine("Press [Enter] to exit.");
+Console.ReadLine();
