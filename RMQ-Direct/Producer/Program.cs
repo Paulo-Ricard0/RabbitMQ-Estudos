afe24164589c1bbf77b97f858baa0e5b4897d09f
Paulo Ricardo M <pr741234@gmail.com>
2024-07-16 11:34:23 -0300

    feat: Implementa produtor da mensagem

---
+++ RMQ-Direct/Producer/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "direct_logs", type: ExchangeType.Direct);
+
+var logLevels = new List<string> { "error", "warning", "info", "warning", "info", "error" };
+
+for (int i = 0; i < 6; i++)
+{
+	string message = $"Log message {i} - Log level: {logLevels[i]}";
+	var body = Encoding.UTF8.GetBytes(message);
+
+	channel.BasicPublish(
+		exchange: "direct_logs",
+		routingKey: $"{logLevels[i]}",
+		basicProperties: null,
+		body: body);
+
+	Console.WriteLine($"[x] Sent {message}");
+}
+
+Console.WriteLine("Press [Enter] to exit.");
+Console.ReadLine();
